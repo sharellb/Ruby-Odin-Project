ae00f0251af906f930ea9265080b64fab6596e3c
Sharell Bryant <sharell.bryant@gmail.com>
2014-06-13 13:34:39 -0400

    first commit

---
+++ stock_picker.rb
@@ -0,0 +1,26 @@
+
+
+def stock_picker(prices)
+
+	biggest_profit = 0
+	buy = 0
+	sell = 0
+
+	new_prices = prices.dup
+	prices.each do |x|
+		new_prices.each do |y|
+		profit = x - y
+			if (profit < biggest_profit)
+				biggest_profit = profit 
+				sell = x
+				buy = y
+			end
+		new_prices.delete(new_prices.first)
+		end
+	end
+	buy = prices.index(buy)
+	sell = prices.index(sell)
+	print "#{sell}, #{buy}"
+end
+
+
