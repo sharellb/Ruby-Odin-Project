e01abd4b64ea820703dbbc9f1ca7064b3eabd8a5
Sharell Bryant <sharell.bryant@gmail.com>
2014-08-25 16:07:51 -0400

    Refactored the game

--- mastermind.rb
+++ mastermind.rb
@@ -2,70 +2,73 @@
 
 puts "Welcome to Mastermind! You will be playing against the computer in this game."
 puts "The computer will choose the order of 4 pegs of 6 different colors (white, yellow, orange, purple, blue, and green)."
-puts "It's up to you to figure out the order and color of the pegs. You get 12 guesses."
-puts "Don't worry. The computer will let you know if you have a peg in the right place or one of the right color...it just won't tell you which peg"
+puts "It's up to you to figure out the order and color of the pegs. Colors can be reused or not used at all. You get 12 guesses."
+puts "Don't worry. The computer will give you feedback after each guess."
 puts "Good luck!"
 puts " "
 
 class MasterMind
-	$colors = ['w','y','o','p','b','g']
+	COLORS = ['w','y','o','p','b','g'] 
 
+	attr_reader :game_over
+	def initialize
+		@turns = 0 
+	end
 	def pick_pegs(choices) 
-		@@pegs = []
+		@pegs = []
 		4.times do
 			peg = choices.sample
-			@@pegs.push(peg)
+			@pegs.push(peg)
 		end
 	end
 
-	@@turns = 0
-	$game = 0
+	
+	@game_over = false
 
 
-	def player_guess
-		puts "Guesses used: #{@@turns}"
+	def player_guess 
+		puts "Guesses used: #{@turns}"
 		puts "Put in your guess separated by commas. Choose from w, y, o, p, b, or g."
-		@@guess = gets.chomp.split(",")
-		if @@guess.length != 4
-			puts "You didn't put in four pegs. Try again!"
+		@guess = gets.chomp.split(",")
+		if @guess.length != 4
+			puts "You didn't put in four pegs. Try again!" 
 			player_guess
 		end
-		@@turns += 1
+		@turns += 1 
 	end
 
-
-	def end_game
-		if @@turns == 12
-			puts "Sorry you're all out of turns :("
-			puts "The order was #{pegs}"
-			$game = 1
-		elsif @@guess == @@pegs
-			puts "YOU WON!"
-			$game = 1
-		end
-	end
-
-	def feedback
-		correct = 0
-		right_color = 0
+	def feedback 
+		correct = 0 
+		right_color = 0 
 		for i in 0..3
-			if @@pegs[i] == @@guess[i]
-				correct = correct + 1
-			elsif @@guess.include? @@pegs[i]
-				right_color = right_color + 1
+			if @pegs[i] == @guess[i] 
+				correct = correct + 1 
+			elsif @guess.include? @pegs[i] 
+				right_color = right_color + 1 
 			end
 		end
 		puts "You have #{correct} in the right place."
 		puts "You have #{right_color} of the right color but not in the correct place."
 		puts " "
 	end
+
+	def end_game
+		if @turns == 12 
+			puts "Sorry you're all out of turns."
+			puts "The order was #{pegs}"
+			@game_over = true
+		elsif @guess == @pegs 
+			puts "YOU WON!"
+			@game_over = true
+		end
+	end
 end
 
 play = MasterMind.new
 
-play.pick_pegs($colors)
+play.pick_pegs(MasterMind::COLORS)
 
-until $game == 1
+until play.game_over
 	play.player_guess
 	play.feedback
 	play.end_game
