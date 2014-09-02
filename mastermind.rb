7aacbc3d9497281532e32f2772139be6d6817b85
Sharell Bryant <sharell.bryant@gmail.com>
2014-09-02 17:57:11 -0400

    User can play multiple games

--- mastermind.rb
+++ mastermind.rb
@@ -1,19 +1,33 @@
 #mastermind
 
-puts "Welcome to Mastermind! You will be playing against the computer in this game."
-puts "The computer will choose the order of 4 pegs of 6 different colors (white, yellow, orange, purple, blue, and green)."
-puts "It's up to you to figure out the order and color of the pegs. Colors can be reused or not used at all. You get 12 guesses."
-puts "Don't worry. The computer will give you feedback after each guess."
-puts "Good luck!"
-puts " "
 
 class MasterMind
 	COLORS = ['w','y','o','p','b','g'] 
 
 	attr_reader :game_over
+	
 	def initialize
 		@turns = 0 
+		rules
+		pick_pegs(COLORS)
+		until game_over
+			player_guess
+			feedback
+			end_game
+		end
+		play_again
 	end
+
+	def rules
+		puts " "
+		puts "Welcome to Mastermind! You will be playing against the computer in this game."
+		puts "The computer will choose the order of 4 pegs of 6 different colors (white, yellow, orange, purple, blue, and green)."
+		puts "It's up to you to figure out the order and color of the pegs. Colors can be reused or not used at all. You get 12 guesses."
+		puts "Don't worry. The computer will give you feedback after each guess."
+		puts "Good luck!"
+		puts " "
+	end
+
 	def pick_pegs(choices) 
 		@pegs = []
 		4.times do
@@ -59,25 +73,38 @@ class MasterMind
 	end
 
 	def end_game
-		if @turns == 12 
+		if @turns == 3
 			puts "Sorry you're all out of turns."
-			puts "The order was #{pegs}"
+			puts "The order was #{@pegs}"
 			@game_over = true
 		elsif @guess == @pegs 
 			puts "YOU WON!"
 			@game_over = true
 		end
+		
+	end
+
+	def play_again
+		puts "Do you want to play again (y/n)?"
+		play_response = gets.chomp
+		if play_response == "y"
+			MasterMind.new
+		elsif play_response == "n"
+			puts "Thanks for playing!"
+		else
+			puts "You have to put y or n"
+			play_again
+		end
 	end
 end
 
-play = MasterMind.new
+MasterMind.new
 
-play.pick_pegs(MasterMind::COLORS)
 
-until play.game_over
-	play.player_guess
-	play.feedback
-	play.end_game
-end
+
+
+
+
+
 
 
